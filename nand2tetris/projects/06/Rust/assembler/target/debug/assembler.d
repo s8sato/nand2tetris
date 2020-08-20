73c3d1af2267c8d436e8fc47ac122d7bdc4ae32b
satu-n <49983831+satu-n@users.noreply.github.com>
2020-08-20 17:36:08 +0900

    ignored /target/

--- nand2tetris/projects/06/Rust/assembler/target/debug/assembler.d
+++
@@ -1,1 +0,0 @@
-/home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/target/debug/assembler: /home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/src/main.rs
--- nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25.d
+++
@@ -1,5 +0,0 @@
-/home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25: src/main.rs
-
-/home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25.d: src/main.rs
-
-src/main.rs:
