f4001dd6499cdace4b48cacb72a4e65ee24708fb
satu-n <49983831+satu-n@users.noreply.github.com>
2020-08-20 15:02:35 +0900

    improving project 06: created Rust version

---
+++ nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25.d
@@ -0,0 +1,5 @@
+/home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25: src/main.rs
+
+/home/satun/study/ep405_nand2tetris/nand2tetris/projects/06/Rust/assembler/target/debug/deps/assembler-590de1679d491c25.d: src/main.rs
+
+src/main.rs:
